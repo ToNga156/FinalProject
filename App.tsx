a56cec58563c30594076a710ef53d76e4ad389d2
ToNga156 <ngaichi2004@gmail.com>
2025-11-04 10:31:48 +0700

    Initial commit - ContactList React Native app

--- App.tsx
+++ App.tsx
@@ -1,45 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import HelloWord from './src/components/demobuoi1/helloWord';
+import DisplayNameAge from './src/components/demobuoi1/displayNameAge';
+import StateDisplayNameAge from './src/components/demobuoi1/stateDisplayNameAge';
+import SetInfor from './src/components/demobuoi1/setInfor';
+import ParentChild from './src/components/demobuoi1/ParentChild';
+import Ptbacnhat from './src/components/demobuoi3/ptbacnhat';
+import CalculatorWithRadio from './src/components/demobuoi3/calculator';
+import BMICalculator from './src/components/demobuoi5/bmi';
+import Shape from './src/components/demobuoi7/shape';
+import ListProduct from './src/components/demobuoi7/listProduct';
+import Parent from './src/components/demobuoi8/Parent';
+import ListProductFlatList from './src/components/demobuoi8/listProductFlatList';
+import StudentManager from './src/components/demobuoi9/mang';
+import Students from './src/components/demobuoi9/students';
+import ContactList from './src/components/buoi10/kiemtra';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+const App = () => {
+  // return <HelloWord />;
+  //  return <DisplayNameAge name="Cọp" />;
+  // return <StateDisplayNameAge />;
+  // return <SetInfor />;
+  // return <ParentChild />;
+  // return <Ptbacnhat />;
+  // return <CalculatorWithRadio />;
+  // return <BMICalculator />;
+  // return <Shape />;
+  // return <ListProduct />;
+  // return <Parent />;
+  // return <ListProductFlatList />;
+  // return <StudentManager />;
+  // return <Students />;
+  return <ContactList />;
+};
 
 export default App;
---
+++ src/components/demobuoi1/abcd.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const abcd = () => {
+  return (
+    <View>
+      <Text>abcd</Text>
+    </View>
+  )
+}
+
+export default abcd
+
+const styles = StyleSheet.create({})
